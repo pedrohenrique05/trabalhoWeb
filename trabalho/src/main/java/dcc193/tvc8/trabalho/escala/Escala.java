0091907c01aff2e71705e9dd5b2281c3be5c869e
Pedro Moura <pedrohenriquedelgado@ice.ufjf.br>
2022-08-08 15:41:13 -0300

    CRUD de escala funcionando as operações básicas porém o radio não esta puxando o valor true/false e consequentemente registrando null no BD!

--- trabalho/src/main/java/dcc193/tvc8/trabalho/escala/Escala.java
+++ trabalho/src/main/java/dcc193/tvc8/trabalho/escala/Escala.java
@@ -4,13 +4,70 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Escala {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
+
+    @NotBlank(message = "E preciso um nome!")
+    String nome;
+
+    @NotBlank(message = "E preciso a url para a sua descrição!")
+    String urlDescricao;
+
+
+    Boolean good;
+
+
+    Boolean badSmells;
+
+
+    
+    public Escala(Long id, @NotBlank(message = "E preciso um nome!") String nome,
+            @NotBlank(message = "E preciso a url para a sua descrição!") String urlDescricao, Boolean good,
+            Boolean badSmells) {
+        this.id = id;
+        this.nome = nome;
+        this.urlDescricao = urlDescricao;
+        this.good = good;
+        this.badSmells = badSmells;
+    }
+
+    
+
+    public Escala(Long id, @NotBlank(message = "E preciso um nome!") String nome,
+            @NotBlank(message = "E preciso a url para a sua descrição!") String urlDescricao, Boolean good) {
+        this(id,nome,urlDescricao,good,null);
+    }
+
+    
+
+
+
+    public Escala(Long id, @NotBlank(message = "E preciso um nome!") String nome,
+            @NotBlank(message = "E preciso a url para a sua descrição!") String urlDescricao) {
+        this(id,nome,urlDescricao,null,null);
+    }
+
+    
+
+    public Escala(Long id, @NotBlank(message = "E preciso um nome!") String nome) {
+        this(id,nome,null,null,null);
+    }
+
+    
+
+    public Escala(Long id) {
+        this(id,null,null,null,null;)
+    }
+
+    public Escala(){
+        this(null,null,null,null,null);
+    }
 
     public Long getId() {
         return id;
@@ -19,6 +76,46 @@ public class Escala {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUrlDescricao() {
+        return urlDescricao;
+    }
+
+    public void setUrlDescricao(String urlDescricao) {
+        this.urlDescricao = urlDescricao;
+    }
+
+    public Boolean getGood() {
+        return good;
+    }
+
+    public void setGood(Boolean good) {
+        this.good = good;
+    }
+
+    public Boolean getBadSmells() {
+        return badSmells;
+    }
+
+    public void setBadSmells(Boolean badSmells) {
+        this.badSmells = badSmells;
+    }
+
+    @Override
+    public String toString() {
+        return "Escala [badSmells=" + badSmells + ", good=" + good + ", id=" + id + ", nome=" + nome + ", urlDescricao="
+                + urlDescricao + "]";
+    }
+
+    
 
     
 }
